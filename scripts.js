8f11ae8cfd99cd646bb68931490add1bc4286994
deadmaruss <deadmaruss@gmail.com>
2018-04-29 12:48:15 +0300

    added text, repositioned page elements

--- scripts.js
+++ scripts.js
@@ -12,3 +12,42 @@ $(document).ready(function(){
 			});
 			});
 });
+
+/*
+$(document).ready(function(){
+			$('.newButton').hover(function() {
+				$(".line").animate({width: '80px'});
+				$(".buttonText").animate({left: '70px'})
+			},
+			function() {
+				$(".line").animate({width: oldWidth});
+				$(".buttonText").animate({right: '70px'})
+			});
+});
+*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
